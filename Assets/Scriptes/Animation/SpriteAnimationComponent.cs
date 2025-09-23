ecf67c95271c3934eef64be3861415c1600924b8
Kostenvo <Cool_crazy@mail.ru>
2025-09-23 22:02:07 +0300

    1.6. Анимация добавил Аниматор, анимации и флипп героя при развороте. Написал SpriteAnimationComponent для проигрывания легких анимаций.

---
+++ Assets/Scriptes/Animation/SpriteAnimationComponent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.PlayerLoop;
+
+namespace Scriptes.Animation
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteAnimationComponent : MonoBehaviour
+    {
+        [SerializeField] [Range(0, 60)] private int _framePerSecond = 10;
+        [SerializeField] private bool _loop = true;
+        [SerializeField] private UnityEvent _onAnimationFinished;
+        [SerializeField] private Sprite[] _sprites;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        private float _frameRate;
+        private float _nextFrameTime;
+        private int _frameIndex;
+
+        private void Awake()
+        {
+            _spriteRenderer = _spriteRenderer ? _spriteRenderer : GetComponent<SpriteRenderer>();
+            RestartAnim();
+        }
+
+        private void Update() => Animeted();
+        private void OnEnable() => RestartAnim();
+
+        public void SetLoopAnimation(bool value) => _loop = value;
+
+        private void Animeted()
+        {
+            if (Time.time < _nextFrameTime) return;
+            if (_frameIndex >= _sprites.Length)
+            {
+                if (_loop)
+                    _frameIndex = 0;
+                else
+                {
+                    enabled = false;
+                    _onAnimationFinished.Invoke();
+                    return;
+                }
+
+            }
+
+            _nextFrameTime = Time.time + _frameRate;
+            _spriteRenderer.sprite = _sprites[_frameIndex];
+            _frameIndex++;
+        }
+
+        private void RestartAnim()
+        {
+            _frameIndex = 0;
+            _frameRate = 1.0f / _framePerSecond;
+            _nextFrameTime = Time.time;
+        }
+    }
+}
--- Assets/Scriptes/Creatures/Hero/HeroMove.cs
+++ Assets/Scriptes/Creatures/Hero/HeroMove.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 
 namespace Scriptes.Creatures.Hero
 {
-    public class HeroMove : MoveBase,IMovable
+    public class HeroMove : MoveBase, IMovable
     {
         [SerializeField] private float moveSpeed = 5f;
         [SerializeField] private float _jampSpeed = 5f;
         [SerializeField] private ChekerSurfaceBase _groundChecker;
+        [SerializeField] private Animator _animator;
         private Vector2 _moveDirection;
         private bool isJamping => _moveDirection.y > 0;
         private Rigidbody2D _rb;
+        private int _moveKey = Animator.StringToHash("Move");
+        private int _jumpKey = Animator.StringToHash("VerticalVelosity");
+        private int _groundCheckKey = Animator.StringToHash("OnGround");
+        private bool _groundCheck => _groundChecker.CheckSurface();
+
 
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
         }
-        
+
 #if UNITY_EDITOR
         private void OnValidate()
         {
-            if (_groundChecker == null)  
+            if (_groundChecker == null)
                 _groundChecker = GetComponentInChildren<ChekerSurfaceBase>();
         }
 #endif
-
         public override void SetDirection(Vector2 dir)
         {
             _moveDirection = dir;
@@ -41,18 +46,31 @@ namespace Scriptes.Creatures.Hero
         {
             if (isJamping)
             {
-                if (_groundChecker?.CheckSurface() == true && _rb.linearVelocity.y < 0.1f)
+                if (_groundCheck == true && _rb.linearVelocity.y < 0.1f)
                     _rb.AddForceY(_jampSpeed, ForceMode2D.Impulse);
             }
             else if (_rb.linearVelocity.y > 0)
             {
                 _rb.linearVelocityY = _rb.linearVelocityY / 2;
             }
+
+            _animator.SetFloat(_jumpKey, _rb.linearVelocity.y);
         }
 
         private void Move()
         {
-                _rb.linearVelocityX = _moveDirection.x * moveSpeed;
+            _rb.linearVelocityX = _moveDirection.x * moveSpeed;
+            SetDirection();
+            _animator.SetBool(_groundCheckKey, _groundCheck);
+            _animator.SetBool(_moveKey, _moveDirection.x != 0);
+        }
+
+        private void SetDirection()
+        {
+            if (_moveDirection.x > 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            else if (_moveDirection.x < 0)
+                transform.localScale = new Vector3(-1, 1, 1);
         }
     }
 }
